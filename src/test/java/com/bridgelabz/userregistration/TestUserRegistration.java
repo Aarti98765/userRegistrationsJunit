b76e9cc0d35439d0c3ede91ffd8051ff599a7760
Aarti98765 <aartiahireengit@gmail.com>
2020-04-05 16:21:13 +0530

    ADDED: Code to enter valid lastname

--- src/test/java/com/bridgelabz/userregistration/TestUserRegistration.java
+++ src/test/java/com/bridgelabz/userregistration/TestUserRegistration.java
@@ -8,15 +8,22 @@ public class TestUserRegistration {
 
         @Test
         public void givenfirstName_whenValid_thenTrue() {
-            boolean firstName = obj.checkName("Capital");
+            boolean firstName = obj.checkName("Aarti");
             Assert.assertTrue(firstName);
         }
 
         @Test
         public void givenfirstName_whenInvalid_thenFalse() {
-            boolean firstName = obj.checkName("Ca");
+            boolean firstName = obj.checkName("Aa");
             Assert.assertFalse(firstName);
         }
+
+       @Test
+        public void givenLastName_whenValid_thenTrue() {
+            boolean lastName = obj.checkName("Ahire");
+            Assert.assertTrue(lastName);
+        }
+
 }
 
 
